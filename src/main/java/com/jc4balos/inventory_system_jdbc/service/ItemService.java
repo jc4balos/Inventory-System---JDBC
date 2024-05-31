b8301725102b0b02ff4fa749e35f13983d3bdb30
Jc Abalos <jc4balos@gmail.com>
2024-05-31 14:32:51 +0800

    add dedcrease qty

--- src/main/java/com/jc4balos/inventory_system_jdbc/service/ItemService.java
+++ src/main/java/com/jc4balos/inventory_system_jdbc/service/ItemService.java
@@ -63,4 +63,14 @@ public class ItemService {
         System.out.println("Item amount updated success");
         con.close();
     }
+
+    public static void decreaseQty(int itemId, int amount) throws SQLException {
+        Connection con = DBConnection.connect();
+        PreparedStatement setItemQty = con.prepareStatement("UPDATE items SET item_qty= item_qty - ? WHERE item_id=?");
+        setItemQty.setInt(1, amount);
+        setItemQty.setInt(2, itemId);
+        setItemQty.executeUpdate();
+        System.out.println("Item amount updated success");
+        con.close();
+    }
 }
